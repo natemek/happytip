15022281fd1d6eaf826a61c898f3762567e19099
natemek <natnaelmek@gmail.com>
2020-08-22 16:12:49 -0400

    Initial Commit

---
+++ happytip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  happytip
+//
+//  Created by Natnael Mekonnen on 8/22/20.
+//  Copyright © 2020 natemek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
